feb7138a95a2205618d76445a83e22731e246719
Suny <suny.mfunl@gmail.com>
2026-08-07 16:14:41 +0530

    Drop hardcoded mfunl.com fallback for APP_URL

--- config/app.php
+++ config/app.php
@@ -8,7 +8,7 @@ return [
     'name' => Env::get('APP_NAME', 'MfunL'),
     'env' => Env::get('APP_ENV', 'production'),
     'debug' => (bool) Env::get('APP_DEBUG', false),
-    'url' => rtrim((string) Env::get('APP_URL', 'https://mfunl.com'), '/'),
+    'url' => rtrim((string) Env::get('APP_URL', ''), '/'),
     'timezone' => Env::get('APP_TIMEZONE', 'Asia/Kolkata'),
     'key' => Env::get('APP_KEY', ''),
 ];
