b9a63a634a34610353aaef40060ab275f2ba80a2
Suny <suny.mfunl@gmail.com>
2026-08-29 13:15:55 +0530

    Switch homepage MADX Awards gallery to webp images
    Also fixes the previously-missing image 6, which now exists in the
    converted set.

--- app/Views/home/sections/14-awards.php
+++ app/Views/home/sections/14-awards.php
@@ -4,8 +4,8 @@
     <p class="section-heading">MADX Awards <span class="mark">2025</span></p>
     <div class="awards__grid">
       <?php for ($i = 1; $i <= 7; $i++): ?>
-        <button type="button" class="awards__item" data-open-lightbox="/assets/images/awards/MADX-Awards-2025<?= $i ?>-img.jpg">
-          <img src="/assets/images/awards/MADX-Awards-2025<?= $i ?>-img-400x284.jpg" alt="MADX Awards 2025 recognition, image <?= $i ?>" title="MADX Awards 2025 recognition, image <?= $i ?>" width="200" height="200" loading="lazy">
+        <button type="button" class="awards__item" data-open-lightbox="/assets/images/awards/MADX-Awards-2025<?= $i ?>-img.webp">
+          <img src="/assets/images/awards/MADX-Awards-2025<?= $i ?>-img-400x284.webp" alt="MADX Awards 2025 recognition, image <?= $i ?>" title="MADX Awards 2025 recognition, image <?= $i ?>" width="200" height="200" loading="lazy">
         </button>
       <?php endfor; ?>
     </div>
